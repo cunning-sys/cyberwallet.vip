0a37a60e9ff17a1f7a729c464002bd47ea5d1037
cunning <76720185+cunning-sys@users.noreply.github.com>
2023-04-02 23:36:13 -0700

    Add files via upload

---
+++ ikyourip/script.js
@@ -0,0 +1,31 @@
+let counter = 0;
+let Title = 'ikyourip';
+let direction = true;
+aniTitle = setInterval(function() {
+    if (counter == Title.length) direction = false;
+    if (counter == false) direction = true;
+    counter = (direction == true) ? ++counter : --counter;
+    newtitle = (counter == 0) ? "" : Title.slice(0, counter);
+    document.title = '' + newtitle;
+}, 400)
+window.oncontextmenu = function() {
+    return false;
+}
+$(document).keydown(function(event) {
+    if (event.keyCode == 123) {
+        return false;
+    } else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+        return false;
+    }
+});
+
+function ap() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.3;
+    audio.play()
+}
+
+function vp() {
+    var video = document.getElementById("video");
+    video.play()
+}
